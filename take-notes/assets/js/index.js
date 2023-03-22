7730e9390e686e34d54bd28e50c59dc46b417cda
Carlos Eduardo <carlos.eduardo.cs@icloud.com>
2023-03-21 22:04:30 -0300

    :recycle: refactor: Alteração de padrão de projeto
    Alterado padrão do projeto de index.js para main, por ser o principal.

--- take-notes/assets/js/index.js
+++
@@ -1,8 +0,0 @@
-import NotesAPI from "./NotesAPI.js";
-import NotesView from "./NotesView.js";
-import App from "./App.Js";
-
-const root = document.getElementById("app");
-const app = new App(root);
-
-
